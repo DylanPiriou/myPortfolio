01c9f4d3ab1fcc441587c520bda475fd1a60010d
DylanPiriou <dylan.piriou@hotmail.com>
2023-03-11 00:49:23 +0100

    dynamic name in navbar done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import "../assets/Style/Navbar.scss";
 
 export default function Navbar() {
+    const location = useLocation();
+    const param = location.pathname.split('/').pop();
+
   return (
     <div className="navbar">
-        <NavLink to="/" className="home-title">@dylan/</NavLink>
+        <NavLink to="/" className="home-title">@dylanpiriou/{param}</NavLink>
         <nav>
-            <NavLink to="/projects">PROJECTS</NavLink>
-            <NavLink to="/contact">CONTACT</NavLink>
+            <NavLink to="/projects" className="link">PROJECTS</NavLink>
+            <NavLink to="/contact" className="link">CONTACT</NavLink>
         </nav>
     </div>
   )
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Navbar from '../Components/Navbar';
+import "../assets/Style/Home.scss";
 
 export default function Home() {
   return (
-    <div>
+    <div className="home">
         <Navbar/>
     </div>
   )
