58c420604a5bc38736ad5b99bd801beb6e3042af
DylanPiriou <dylan.piriou@hotmail.com>
2023-03-11 01:25:30 +0100

    first part of home page done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,6 +6,14 @@ export default function Home() {
   return (
     <div className="home">
         <Navbar/>
+        <img src="main-hero.jpg" className="main-img" alt="person from the back looking at the horizon" />
+        <h1>
+            <span>Hello,</span>
+            <br />
+            <span>Im a FrontEnd</span>
+            <br />
+            <span>Developer</span>
+        </h1>
     </div>
   )
 }
