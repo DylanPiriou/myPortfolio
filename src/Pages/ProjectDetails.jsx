73af4358bbaa40899570de6b00e0ca892b43b472
DylanPiriou <dylan.piriou@hotmail.com>
2023-03-12 18:40:39 +0100

    first try for project page

--- src/Pages/ProjectDetails.jsx
+++ src/Pages/ProjectDetails.jsx
@@ -2,17 +2,42 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import Data from "../Data/data.json";
 import "../assets/Style/ProjectDetails.scss";
+import Navbar from '../Components/Navbar';
 
 export default function ProjectDetails() {
     const { id } = useParams();
     const projectsData = Data.find(project => project.id === +id);
+
   return (
     <div className="projectDetails">
-        <h1>{projectsData.title}</h1>
-        <img src={projectsData.cover} alt="" />
-        <div className="imgs-container">
-          <img src="" alt="" />
-          <div className="imgs-gallery">
+      <Navbar/>
+        <h2>{projectsData.title}</h2>
+        <div className="project-container">
+          <div className="project-img">
+            <img src={projectsData.cover} alt="" />
+          </div>
+          <div className="project-infos">
+            <div className="title-box">
+              <h3>Lorem ipsum dolor sit amet.</h3>
+              <span>{projectsData.date}.</span>
+            </div>
+            <div className="tags-box">
+              <div className="tag">HTML</div>
+              <div className="tag">CSS</div>
+              <div className="tag">JS</div>
+            </div>
+            <ul>
+              <li>gggggg</li>
+              <li>ggggggggg</li>
+              <li>ggggggg</li>
+              <li>ggggggg</li>
+              <li>gggggggggg</li>
+            </ul>
+            <p>{projectsData.description}</p>
+            <div className="buttons-box">
+              <button>Contact me</button>
+              <button className="github-btn">Check on GitHub</button>
+            </div>
           </div>
         </div>
     </div>
