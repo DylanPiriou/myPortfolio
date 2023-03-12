96444203d7d377aa990df1dba1b9f0c28d03ccf9
DylanPiriou <dylan.piriou@hotmail.com>
2023-03-12 17:28:40 +0100

    some adjusts on css

--- src/Pages/ProjectDetails.jsx
+++ src/Pages/ProjectDetails.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import Data from "../Data/data.json";
+import "../assets/Style/ProjectDetails.scss";
 
 export default function ProjectDetails() {
     const { id } = useParams();
@@ -9,6 +10,11 @@ export default function ProjectDetails() {
     <div className="projectDetails">
         <h1>{projectsData.title}</h1>
         <img src={projectsData.cover} alt="" />
+        <div className="imgs-container">
+          <img src="" alt="" />
+          <div className="imgs-gallery">
+          </div>
+        </div>
     </div>
   )
 }
